ab794b544e337f2c4d86ed088780bfdc275339c4
Will Ballard <wballard@mailframe.net>
2019-03-17 19:27:42 +0000

    bag of words sentiment model

---
+++ 6-2.py
@@ -0,0 +1,63 @@
+# %%[markdown]
+import spacy
+import torch
+My favorite way to parse these days - -at least when I 'm working
+with words - -is spacy, the Dockerfile includes english support, so
+let 's parse!
+
+# %%
+nlp = spacy.load('en')
+
+# %%[markdown]
+Now parse some words from a sentence.
+
+# %%
+parsed = nlp('Hello there my jupyter notebook!')
+for token in parsed:
+    print(token)
+
+# %%[markdown]
+Bag of words is like one hot encoding when we are talking about
+single words - -and the first thing you need to do to encode
+is turn words into ordinals.
+
+# %%
+ordinals = {}
+for token in parsed:
+    if token.text not in ordinals:
+        ordinals[token.text] = len(ordinals)
+ordinals
+
+# %%[markdown]
+And now one hot encoding is simply a matter of 'turning on' the
+part of a tensor when a word matches - -let 's look at a single word.
+
+# %%
+word_one_hot = torch.zeros(len(ordinals))
+word_one_hot[ordinals['there']] = 1
+word_one_hot
+
+# %%[markdown]
+And here is the interesting part - -while language clearly has order
+and is a sequence, it is often good enough to ignore this and encode
+all the words into a vector - -this is the bag - of - words.All the words
+in the bag lose their order, so we simply count the number of times a word
+appears and encode that in a tensor.
+
+# %%
+sentence_bag_of_words = torch.zeros(len(ordinals))
+for token in parsed:
+    sentence_bag_of_words[ordinals[token.text]] += 1
+
+sentence_bag_of_words
+
+# %%[markdown]
+Now this is a bit of a silly example, since all the words are turned on,
+but when you apply this technique to a list of texts, each with difference words
+you can get a working input model.
+
+This may seem like a really gross simplification of language - -and it is
+- -and we will look to more advanced methods very soon, but for now we will
+try to classify text with just this really simple method.
+
+
---
+++ 6-3.py
@@ -0,0 +1,120 @@
+# %%
+import torch.utils.data
+import sklearn.metrics
+import torch
+import pandas
+from torch.utils.data import Dataset
+import tqdm
+import spacy
+nlp = spacy.load('en')
+
+# %%
+pandas.read_csv('sentiment.tsv', sep='\t', header=0).iloc[0]
+
+# %%
+class SentimentDataset(Dataset):
+    def __init__(self):
+        self.data = pandas.read_csv('sentiment.tsv', sep='\t', header=0)
+        # one hot encoding prep
+        self.ordinals = {}
+        for sample in tqdm.tqdm(self.data.Phrase):
+            for token in nlp(sample, disable=['parser', 'tagger', 'ner']):
+                if token.text not in self.ordinals:
+                    self.ordinals[token.text] = len(self.ordinals)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if type(idx) is torch.Tensor:
+            idx = idx.item()
+        sample = self.data.iloc[idx]
+        bag_of_words = torch.zeros(len(self.ordinals))
+        for token in nlp(sample.Phrase, disable=['parser', 'tagger', 'ner']):
+            bag_of_words[self.ordinals[token.text]] += 1
+
+        return bag_of_words, torch.tensor(sample.Sentiment, dtype=torch.float)
+
+
+sentiment = SentimentDataset()
+
+# %%
+sentiment[0]
+
+
+# %%
+# break this into a training and testing dataset
+number_for_testing = int(len(sentiment) * 0.05)
+number_for_training = len(sentiment) - number_for_testing
+train, test = torch.utils.data.random_split(sentiment,
+                                            [number_for_training, number_for_testing])
+trainloader = torch.utils.data.DataLoader(
+    train, batch_size=32, shuffle=True)
+testloader = torch.utils.data.DataLoader(
+    test, batch_size=32, shuffle=True)
+
+len(test), len(train)
+
+#%%
+# Now we have a regression problem! We 'll try to predict the sentiment
+# using our approach for simple regression from earlier videos.
+
+
+# %%
+class Model(torch.nn.Module):
+
+    def __init__(self, input_dimensions, size=128):
+        super().__init__()
+        self.norm_one = torch.nn.BatchNorm1d(input_dimensions)
+        self.layer_one = torch.nn.Linear(input_dimensions, size)
+        self.activation_one = torch.nn.ReLU()
+        self.norm_two = torch.nn.BatchNorm1d(size)
+        self.layer_two = torch.nn.Linear(size, size)
+        self.activation_two = torch.nn.ReLU()
+        self.shape_outputs = torch.nn.Linear(size, 1)
+
+    def forward(self, inputs):
+        buffer = self.norm_one(inputs)
+        buffer = self.layer_one(buffer)
+        buffer = self.activation_one(buffer)
+        buffer = self.norm_two(buffer)
+        buffer = self.layer_two(buffer)
+        buffer = self.activation_two(buffer)
+        buffer = self.shape_outputs(buffer)
+        return buffer
+
+
+model = Model(len(sentiment.ordinals))
+
+
+# %%
+optimizer = torch.optim.Adam(model.parameters())
+loss_function = torch.nn.MSELoss()
+model.train()
+for epoch in range(4):
+    losses = []
+    for inputs, outputs in tqdm.tqdm(trainloader):
+        optimizer.zero_grad()
+        results = model(inputs)
+        loss = loss_function(results, outputs)
+        losses.append(loss.item())
+        loss.backward()
+        optimizer.step()
+    print("Loss: {0}".format(torch.tensor(losses).mean()))
+
+# %%
+import numpy as np
+results_buffer = []
+actual_buffer = []
+with torch.no_grad():
+    model.eval()
+    for inputs, outputs in testloader:
+        results = model(inputs).detach().numpy()
+        actual = outputs.numpy()
+        results_buffer.append(results)
+        actual_buffer.append(actual)
+
+
+print(sklearn.metrics.r2_score(
+    np.concatenate(actual_buffer),
+    np.concatenate(results_buffer)))
